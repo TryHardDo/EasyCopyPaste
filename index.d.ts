57ff871a92d876c04fcd9ce3aa7080aca48f9443
TryHardDo <levai.levente2002@gmail.com>
2023-06-16 14:11:12 +0200

    Pushing fresh builds

--- index.d.ts
+++ index.d.ts
@@ -10,24 +10,28 @@ export default class EasyCopyPaste {
     private mapCache;
     constructor(mapFileName: string, mapFileLocation: string);
     /**
-     * Method to convert items to easy copy paste string.
-     * If it is an exceptional item with special delimeter(s) in it's name
-     * we map the item to be back convertable and save it to a file where the special
-     * item names are located.
+     * Method to convert item names into easily copy-pasteable strings.
      *
-     * If the item does not have special delimeter just spaces we wont save the item since it is
-     * easily reverseable.
-     * @param str The plain item name
-     * @returns The parsed item name string
+     * If an item is considered 'exceptional' (i.e., its name contains special delimiters such as ',' '.' '-'),
+     * this method will replace those delimiters too and saves the string pair to an array where later
+     * we can retrieve it.
+     *
+     * If the item's name contains only spaces as delimiters (i.e., it's not 'exceptional'),
+     * we don't store the string pair because such strings are easily reversible.
+     *
+     * @param {string} str The original item name to be parsed.
+     * @returns {string} The parsed item name, ready for easy copying and pasting.
      */
     toEasyCopyPasteString(str: string): string;
     /**
-     * Method to convert easy copy paste string back to the item's original format.
-     * If the item is in the special mapped item names we will return the stored original name.
+     * Method to convert an easily copy-pasteable string back to the original format of the item's name.
      *
-     * If the item is not in the special item names mape we just replace the underscores to spaces.
-     * @param str The easy copy paste string
-     * @returns The original item name
+     * If the string corresponds to an item in the 'special' mapped item names, the method returns the stored original name.
+     *
+     * If the string does not correspond to a 'special' item name, the method replaces underscores with spaces.
+     *
+     * @param {string} str The easy copy-paste string.
+     * @returns {string} The original format of the item's name.
      */
     fromEasyCopyPasteString(str: string): string;
     private findMappedValue;
